98d08bfad92474ffa1a4485131a917341de2ebe7
Ebenezer Asamoah <ebenezer@giantswarm.io>
2025-04-14 09:15:00 +0200

    resolved linting errors

--- internal/controller/policyexception_controller.go
+++ internal/controller/policyexception_controller.go
@@ -22,7 +22,6 @@ import (
 
 	policyAPI "github.com/giantswarm/policy-api/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
@@ -37,6 +36,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/giantswarm/kyverno-policy-operator/internal/utils"
+)
+
+// Add these constants at the package level, after imports
+const (
+	KindDeployment = "Deployment"
+	KindReplicaSet = "ReplicaSet"
+	KindCronJob    = "CronJob"
+	KindJob        = "Job"
+	KindPod        = "Pod"
 )
 
 // PolicyExceptionReconciler reconciles a PolicyException object
@@ -63,7 +71,7 @@ func (r *PolicyExceptionReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		// Error fetching the report
 
 		// Check if the PolicyException was deleted
-		if apierrors.IsNotFound(err) {
+		if errors.IsNotFound(err) {
 			// Ignore
 			return ctrl.Result{}, nil
 		}
@@ -157,21 +165,20 @@ func (r *PolicyExceptionReconciler) CreateOrUpdate(ctx context.Context, obj clie
 
 // generateKinds creates the subresources necessary for top level controllers like Deployment or StatefulSet
 func generateExceptionKinds(resourceKind string) []string {
-	// Adds the subresources to the exception list for each Kind
-	var exceptionKinds []string
-	exceptionKinds = append(exceptionKinds, resourceKind)
-	// Append ReplicaSets
-	if resourceKind == "Deployment" {
-		exceptionKinds = append(exceptionKinds, "ReplicaSet")
-		// Append Jobs
-	} else if resourceKind == "CronJob" {
-		exceptionKinds = append(exceptionKinds, "Job")
-	}
-	// Always append Pods except if they are the initial resource Kind
-	if resourceKind != "Pod" {
-		exceptionKinds = append(exceptionKinds, "Pod")
+	exceptionKinds := []string{resourceKind}
+
+	switch resourceKind {
+	case KindDeployment:
+		exceptionKinds = append(exceptionKinds, KindReplicaSet)
+	case KindCronJob:
+		exceptionKinds = append(exceptionKinds, KindJob)
+	case KindPod:
+		// Special case: if resourceKind is Pod, don't add Pod again
+		return exceptionKinds
 	}
 
+	// For all resource kinds except Pod, add Pod
+	exceptionKinds = append(exceptionKinds, KindPod)
 	return exceptionKinds
 }
 
